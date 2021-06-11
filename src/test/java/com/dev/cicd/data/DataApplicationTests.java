12d93227145b6b4d2b12a7833af2f0b7ce61bfeb
Gokalp <grencber@gmail.com>
2021-06-11 09:10:16 +0300

    First commit for CI/CD pipeline

---
+++ src/test/java/com/dev/cicd/data/DataApplicationTests.java
@@ -0,0 +1,41 @@
+package com.dev.cicd.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class DataApplicationTests {
+
+	@Autowired
+	DataController dataController;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void health() {
+		assertEquals( "Health check is OK", dataController.healthCheck());
+	}
+
+	@Test
+	void version() {
+		assertEquals( "The actual version is 1.0.0", dataController.version());
+	}
+
+	@Test
+	void nationsLength() {
+		Integer nationsLength = dataController.getRandomNations().size();
+		assertEquals(10, nationsLength);
+	}
+
+	@Test
+	void currenciesLength() {
+		Integer currenciesLength = dataController.getCurrencies().size();
+		assertEquals(20, currenciesLength);
+	}
+}
